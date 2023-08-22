f7bc5b2f33abd8a94c3dc18f9c1cde0b4e1f02f1
ske7 <ske777@gmail.com>
2023-08-22 14:58:37 +0300

    Update build strategy

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,21 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import preload from 'vite-plugin-preload';
+import VitePluginInjectPreload from 'vite-plugin-inject-preload';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), preload()],
+  plugins: [
+    vue(),
+    preload(),
+    VitePluginInjectPreload({
+      files: [
+        {
+          match: /[a-z-0-9]*.(css)$/i
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
