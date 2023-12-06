f4ca6a787155ca2a29bdd81ebd22d20983cc8a86
ske7 <ske777@gmail.com>
2023-12-07 01:32:58 +0200

    Change split chunks strategy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import preload from 'vite-plugin-preload';
 import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
@@ -8,6 +8,7 @@ import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 export default defineConfig({
   plugins: [
     vue(),
+    splitVendorChunkPlugin(),
     preload({
       includeCss: false
     }),
