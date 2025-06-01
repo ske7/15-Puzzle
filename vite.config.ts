e99172eb8f38faf3a66a4c913aba36ff16584dc2
ske7 <ske777@gmail.com>
2025-06-01 14:35:44 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import preload from 'vite-plugin-preload';
 import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue')) {
+              return 'vue';
+            }
+            return 'vendor';
+          }
+        }
+      },
+    },
+  },
   plugins: [
     vue(),
-    splitVendorChunkPlugin(),
     preload({
       includeCss: false
     }),
